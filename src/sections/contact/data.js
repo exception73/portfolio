c6cc10fba325e120757f3bef698b3210dbb3ef17
Gautam Khatri <109818831+exception73@users.noreply.github.com>
2024-05-16 00:56:32 +0530

    Update data.js

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,10 +1,20 @@
 import {HiOutlineMail} from 'react-icons/hi'
 import {RiMessengerLine} from 'react-icons/ri'
 import {FaWhatsapp} from 'react-icons/fa'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineLinkedin} from "react-icons/ai";
+import {AiFillGithub} from 'react-icons/ai'
+import {SiLeetcode,SiCodeforces} from 'react-icons/si';
+import {FaWhatsapp} from 'react-icons/fa'
 
 const data = [
     {id: 1, icon: <HiOutlineMail/>, link: 'mailto:buddhgautam777@gmail.com'},
-    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+917404659405'}
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+917404659405'},
+    {id: 4, link: 'https://twitter.com/gautamkhatri05', icon: <AiOutlineTwitter/>},
+    {id: 5, link: 'http://linkedin.com/in/gautambuddh/', icon: <AiOutlineLinkedin/>},
+    {id: 6, link: 'https://github.com/exception73', icon: <AiFillGithub/>},
+    {id:7, link: 'https://leetcode.com/exception73', icon: <SiLeetcode/> },
+    {id:8, link: 'https://codeforces.com/profile/exception73', icon: <SiCodeforces/> }
 ]
 
 
